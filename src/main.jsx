f1e394c8670c99946017922c30cbe22f76e8e329
mdirfan17-code <mohammedirfan170906@gmail.com>
2026-08-04 13:32:36 +0530

    Configure GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./styles/variables.css";
 import "./styles/global.css";
@@ -10,8 +10,8 @@ import App from "./App";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+       <HashRouter>
+    <App />
+</HashRouter>
     </StrictMode>
 );
